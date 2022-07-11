8ff576614b6742803e494c0a890ca8bd850932b6
James Sinkala <xinnks@gmail.com>
2022-07-11 21:28:14 +0300

    improvement: Add better annotations

--- routes/magicLinks.js
+++ routes/magicLinks.js
@@ -1,5 +1,4 @@
-const express = require("express")
-const router = express.Router()
+const router = require("express").Router()
 const { appAuthentication } = require("./../middlewares")
 const Cors = require("cors")
 const corsOptions = {
