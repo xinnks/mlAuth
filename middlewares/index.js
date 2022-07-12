47f0e6d4e7dc87928aa7b9d8b2ee96f1d01e7e0c
James Sinkala <xinnks@gmail.com>
2022-07-12 21:35:42 +0300

    improvement: Update middlewares - configure functions to work with new db setup - Use better annotations

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,58 +1,45 @@
 const { basicAuthentication } = require("./../auth")
 const { comparePasswordHashes, result } = require("./../utils")
-const { appSalt1, appClientIndex, appRefIdIndex } = require("./../vars")
-const Database = require("./../db")
+const { appSalt1, mlauthServiceClient } = require("./../vars")
 const Session = require("./../auth/session")
-const db = new Database()
+const appDb = require("./../db/apps")
 
-// authenticates concurrent service requests on the mlAuth client
+/**
+ * @description Authenticates concurrent HTTP requests on the mlAuth client
+ */
 async function serviceSessionAuthentication(req, res, next) {
-  let { body, headers, url } = req
+  let { sessionToken } = req.body
 
-  if (url === "/register" || url === "/verify") return next()
-
-  if (!body.sessionToken)
+  if (!sessionToken)
     return res.status(403).json({
       message: "Authentication failed",
     })
 
   let { status: sessionStatus, data: sessionData } = await new Session(
-    body.sessionToken
+    sessionToken
   ).verify()
 
   if (sessionStatus !== "success")
     return res.status(203).json({
-      message: "session expired",
+      message: `Encountered error trying to verify session. [${sessionData}]`,
     })
 
-  let {
-    refId,
-    data: { appRefId },
-  } = sessionData
-  
-  const { status: appStatus, data: appData } = await db.find(
-    appRefIdIndex,
-    appRefId
-  )
-  if (appStatus !== "success" || !appData) return result("failure", appData)
+  let { user } = sessionData
 
   req.body = {
     ...req.body,
-    session: {
-      ...sessionData,
-    },
-    account: {
-      ...Object.assign(appData.data, { refId: appData.refId }),
-    },
+    session: sessionData,
+    account: user,
   }
 
   next()
 }
 
-// authenticates apps trying to make magic link requests & verification
+/**
+ * @description Authenticates apps trying to use the service to make magic link requests & verifications
+ * @returns 
+ */
 async function appAuthentication(req, res, next) {
-  let { body, headers, url, method } = req
-
   let { status: bAStatus, data: credentials } = basicAuthentication(req)
   if (bAStatus !== "success")
     return res.status(401).json({
@@ -61,21 +48,31 @@ async function appAuthentication(req, res, next) {
 
   let { client, secret } = credentials
 
-  const { status, data: response } = await db.find(appClientIndex, client)
-  if (status !== "success" || !response) return result("failure", response)
+  const { status: appFindStatus, data: appInfo } = await appDb.findSingleApp(
+    { client },
+    { owner: true }
+  )
+  if (appFindStatus !== "success" || !appInfo)
+    return res.status(404).json({
+      message: "App not found",
+    })
 
-  let { data, refId } = response
-  if (!comparePasswordHashes(secret, data.secret, appSalt1))
+  let { secret: storedSecret } = appInfo
+  if (!comparePasswordHashes(secret, storedSecret, appSalt1))
     return res.json({
       message: "Wrong credentials. Failed to authenticate service",
     })
 
-  let userData = data.user
-  delete data.secret
-  delete data.user
+  // return app's user data when in mlAuth front-end client
+  if (appInfo.client === mlauthServiceClient) {
+    req.body.account = appInfo.owner
+  }
+  delete appInfo.secret
+  delete appInfo.owner
+
   req.body = {
     ...req.body,
-    account: Object.assign(data, { ...userData, refId }),
+    app: appInfo,
   }
 
   return next()
